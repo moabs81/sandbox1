5eb6eefe53a991ff73d967fad98b40114a99a3c2
Nick Hendricks <moabs81@hotmail.com>
2017-09-26 20:26:10 -0400

    created a table

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,47 @@
 console.log('\'Allo \'Allo!');
+var data = {
+    item1 : {
+        title : "Item 1",
+        description : "this is an item",
+        date : "08/25/2017",
+        severe : "no"
+    },
+    item2 : {
+        title : "Item 2",
+        description : "this is an item",
+        date : "09/25/2017",
+        severe : "no"
+    },
+    item3 : {
+        title : "Item 3",
+        description : "this is an item",
+        date : "10/25/2017",
+        severe : "yes"
+    },
+    item4 : {
+        title : "Item 4",
+        description : "this is an item",
+        date : "11/25/2017",
+        severe : "no"
+    },
+    headers : ['title','description','date','severe'],
+    count : 4
+}
+$(window).on('load',function(){
+    console.log(data);
+    var arrayObjects = [];
+    for(var i=0; i < data.headers.length; i++){
+        $(".table-header-target").append('<th>' + data.headers[i] + '</th>');
+    };
+    for(var each in data){
+        arrayObjects.push(each);
+    };
+    for(var i=0; i < data.count; i++){
+        if(data[arrayObjects[i]].severe == "yes"){
+            $(".table-body-target").append('<tr class = "danger"><td>' + data[arrayObjects[i]].title + '</td><td>' + data[arrayObjects[i]].description + '</td><td>' + data[arrayObjects[i]].date + '</td><td>' + data[arrayObjects[i]].severe + '</td></tr>')
+        }else{
+        $(".table-body-target").append('<tr><td>' + data[arrayObjects[i]].title + '</td><td>' + data[arrayObjects[i]].description + '</td><td>' + data[arrayObjects[i]].date + '</td><td>' + data[arrayObjects[i]].severe + '</td></tr>')
+        }
+    };
+    
+})
